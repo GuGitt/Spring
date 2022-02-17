5bbb6bd3c098aafb9386d22dd0006f942af00641
Gustavo <gugul5@hotmail.com>
2022-02-17 11:52:18 -0300

    Liçoes de Spring

---
+++ ExercHelloWorld/src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.generation.helloword.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+
+public class HelloController {
+	
+		
+	@GetMapping
+	
+	public String getHello() {
+		return " Habilidades e mentalidades usadas: PersistÃªncia, Orientação ao Futuro e adaptalidade";
+	}
+	
+	@GetMapping ("/Semana")
+	
+	public String getHello2 () {
+		return "Desenvolver melhor minhas habilidades em MySQL e Java ";
+		
+		
+	}
+		
+	}
+	
+
